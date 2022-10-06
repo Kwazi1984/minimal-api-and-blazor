e3aa5d0832a0e0b30a1906a98c9f72f63a656d52
Lukasz Jedrusik <lukasz.jedrusik@canpack.eu>
2022-10-06 19:24:59 +0200

    minimal-api-and-blazor #26 Documants.Api namespacve changed

--- Documents.Api/Migrations/DataContextModelSnapshot.cs
+++ Documents.Api/Migrations/DataContextModelSnapshot.cs
@@ -2,18 +2,16 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace MinimalApi.Migrations
+namespace Documents.Api.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20220907181137_InitialMigration")]
-    partial class InitialMigration
+    partial class DataContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.8");
